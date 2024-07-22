a13aaf1309cdf80067147cacf5d372fb98cfdc43
Nazarii <dzhaman.nazar2003@gmail.com>
2024-07-22 11:06:21 +0300

    improve header style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import css from './Header.module.css';
 export const Header: FC = () => {
 	return (
 		<header>
-			<div>
+			<div className={css.header_menu}>
 				<span className={css.header_logo}>House Staff</span>
 				<ul className={css.header_list}>
 					<li className={css.header_item}>About us</li>
