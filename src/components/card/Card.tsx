4cee06b10111892f89867c04a023e9925b62dd87
Nazarii <dzhaman.nazar2003@gmail.com>
2024-07-25 11:39:00 +0300

    improve card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,23 +2,36 @@ import { FC } from "react";
 import { IData } from "../../data";
 import css from "./Card.module.css";
 import heartSvg from "../../svg/heart.svg";
+import basketSvg from "../../svg/basket.svg";
 
 export const Card: FC<IData> = ({ title, img, desc, price }) => {
 	return (
 		<>
-			<div className={css.card_photo}>
 			<img
 				className={css.card_img}
 				src={img}
 				alt={title}
 			/>
-			<img className={css.card_heart} src={heartSvg} alt="heart" />
-			</div>
 			<div className={css.card_container}>
 				<h2 className={css.card_title}>{title}</h2>
 				<p className={css.card_desc}>{desc}</p>
-				<b className={css.card_price}>{price}$</b>
-				<p className={css.card_basket}>+</p>
+				<ul className={css.card_icons}>
+					<li>
+						<b className={css.card_price}>{price}$</b>
+					</li>
+					<li className={css.card_icon}>
+						<img
+							src={basketSvg}
+							alt="basket"
+						/>
+					</li>
+					<li className={css.card_icon}>
+						<img
+							src={heartSvg}
+							alt="heart"
+						/>
+					</li>
+				</ul>
 			</div>
 		</>
 	);
